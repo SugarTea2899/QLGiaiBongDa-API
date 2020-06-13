b84c057faf335ce2946e3663eb11796ccf1197aa
Thanh Tien <tiennt499@gmail.com>
2020-06-13 11:09:39 +0700

    + Thêm chức năng chỉnh sửa thông tin trọng tài, tìm kiếm theo tên, lấy thông tin bằng id

--- API/controller/referee.js
+++ API/controller/referee.js
@@ -42,12 +42,75 @@ module.exports = {
             }
 
             await refereeDB.findOneAndDelete({_id: refereeID});
-            res.status(404).json({
+            res.status(200).json({
                 message: "Removing referee is successful"
             });
         } catch(e) {
             res.status(404).json({
                 message: "Referee ID not found"
+            });
+        }
+    },
+
+    update: async function(req, res, next) {
+        try {
+            const id = req.body.refereeId;
+            if (id === undefined) {
+                res.status(404).json({
+                    message: "referee id is undefined"
+                });
+                return;
+            }
+
+            const aReferee = await refereeDB.findById(id);
+            for (member in req.body) {
+                if (member != "refereeId") {
+                    aReferee[member] = req.body[member];
+                }
+            }
+
+            await aReferee.save();
+            res.status(200).json({
+                message: "updating referee is successful"
+            });
+        } catch(e) {
+            res.status(404).json({
+                message: "referee id is not found"
+            });
+        }
+    },
+
+    search: async function(req, res, next) {
+        try {
+            let val = req.query.name;
+            if (val === undefined) {
+                val = "";
+            }
+
+            const list = await refereeDB.find({name: {"$regex": val, "$options": "i"}});
+            res.status(200).json(list);
+        } catch(e) {
+            res.status(404).json({
+                message: e.message
+            });
+        }
+    },
+
+    getInfo: async function(req, res, next) {
+        try {
+            const refereeId = req.query.refereeId;
+            if (refereeId === undefined) {
+                res.status(404).json({
+                    message: "referee id is undefined"
+                });
+                return;
+            }
+
+            const referee = await refereeDB.findById(refereeId);
+            res.status(200).json(referee);
+        } catch(e) {
+            res.status(404).json({
+                message: "referee id is not found"
             });
         }
     }
--- API/routes/referee.js
+++ API/routes/referee.js
@@ -12,4 +12,19 @@ router.post('/remove', async function(req, res, next){
     refereeController.remove(req, res, next)
 });
 
+/* update referee */
+router.post('/update', async function(req, res, next){
+    refereeController.update(req, res, next)
+});
+
+/* search referee list by name */
+router.get('/search', async function(req, res, next){
+    refereeController.search(req, res, next);
+});
+
+/* get referee's info */
+router.get('/info', async function(req, res, next){
+    refereeController.getInfo(req, res, next);
+});
+
 module.exports = router;
