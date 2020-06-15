9dfc62383154d1d24e5b1cd7f20f000f256c3b12
LeTriThong <lttvip1999@gmail.com>
2020-06-15 11:45:43 +0700

    + Chức năng xóa đội, cập nhật thông tin đội và tìm kiếm theo tên đội

--- API/controller/team.js
+++ API/controller/team.js
@@ -34,6 +34,65 @@ module.exports = {
                 message: e.message
             })
         }
+    },
+    remove: async function(req, res, next){
+        try{
+            const teamId = req.body.teamId;
+            if (teamId === undefined){
+                res.status(404).json({
+                    message: "Team ID is undefined."
+                });
+                return;
+            }
+            await teamDB.findOneAndDelete({_id: teamId});
+            res.status(200).json({
+                message: "Team deleted successfully"
+            });
+        }catch(e){
+            res.status(404).json({
+                message: "Team not found"
+            });
+        }
+    },
+    search: async function(req, res, next){
+        try{
+            let val = req.query.name;
+            if (val === undefined)
+                val = "";
 
+            const list = await teamDB.find({name: {"$regex": val, "$options": "i"}});
+            res.status(200).json(list);
+        }catch(e){
+            res.status(404).json({
+                message: e.message
+            });
+        }
+    },
+    update: async function(req, res, next){
+        try{
+            const id = req.body.teamId;
+            if (id === undefined){
+                res.status(404).json({
+                    message: "teamID is undefined"
+                });
+                return;
+            }
+            const team = await teamDB.findById(id); //catch below
+
+            for (key in req.body){
+                if (key != "teamId"){
+                    team[key] = req.body[key];
+                }
+            }
+
+            await team.save();
+            res.status(200).json({
+                message: "Team info updated successfully"
+            });
+        }catch(e){
+            res.status(404).json({
+                message: "Team not found"
+            });
+        }
     }
 }
--- API/routes/team.js
+++ API/routes/team.js
@@ -7,4 +7,19 @@ router.post('/add', async function(req, res, next){
     teamController.add(req, res, next);
 });
 
+/*remove team*/
+router.post('/remove', async function(req, res, next){
+    teamController.remove(req, res, next);
+});
+
+/*update team*/
+router.post('/update', async function(req, res, next){
+    teamController.update(req, res, next);
+});
+
+/*search team by name*/
+router.get('/search', async function(req, res, next){
+    teamController.search(req, res, next);
+});
+
 module.exports = router;
